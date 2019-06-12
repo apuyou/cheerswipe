39b1a0148507042f424e6c319bd6544f93177c86
Arthur Puyou <arthur@puyou.fr>
2019-06-12 17:32:10 +0200

    Add Apollo caching

--- App.js
+++ App.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import {
+  Platform,
+  StatusBar,
+  StyleSheet,
+  View,
+  AsyncStorage,
+  Text,
+} from 'react-native';
 import { AppLoading, Asset, Font, Icon, SecureStore } from 'expo';
 import ApolloClient from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { persistCache } from 'apollo-cache-persist';
 import { createHttpLink } from 'apollo-link-http';
 import { ApolloProvider } from 'react-apollo';
 
@@ -15,12 +23,39 @@ export default class App extends React.Component {
   state = {
     isLoadingComplete: false,
     authToken: null,
+    client: null,
   };
 
   async componentDidMount() {
     const authToken = await SecureStore.getItemAsync(KEY_ACCESS_TOKEN);
     if (authToken) {
       this.setState({ authToken });
+    }
+  }
+
+  async componentDidUpdate(prevProps, prevState) {
+    const { authToken } = this.state;
+    if (prevState.authToken !== authToken) {
+      const cache = new InMemoryCache();
+      await persistCache({
+        cache,
+        storage: AsyncStorage,
+      });
+
+      const client = new ApolloClient({
+        link: createHttpLink({
+          uri: 'https://api.producthunt.com/v2/api/graphql',
+          headers: { Authorization: `Bearer ${authToken}` },
+        }),
+        cache: new InMemoryCache(),
+        defaultOptions: {
+          query: {
+            fetchPolicy: 'cache-first',
+          },
+        },
+      });
+
+      this.setState({ client });
     }
   }
 
@@ -40,19 +75,18 @@ export default class App extends React.Component {
           <SigninScreen setAuthToken={this.setAuthToken} />
         </View>
       );
-    } else {
-      const client = new ApolloClient({
-        link: createHttpLink({
-          uri: 'https://api.producthunt.com/v2/api/graphql',
-          headers: { Authorization: `Bearer ${this.state.authToken}` },
-        }),
-        cache: new InMemoryCache(),
-      });
-
+    } else if (!this.state.client) {
       return (
         <View style={styles.container}>
           {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <ApolloProvider client={client}>
+          <Text>Loading…</Text>
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+          <ApolloProvider client={this.state.client}>
             <AppNavigator />
           </ApolloProvider>
         </View>
